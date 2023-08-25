d32510cbb12d92cfe130ecd02e42184ea9dce13d
OlegBEZb <olegolego@mail.ru>
2023-08-25 14:27:29 +0200

    small changes

--- src/common/get_project_root.py
+++ src/common/get_project_root.py
@@ -6,4 +6,4 @@ logger = logging.getLogger(__name__)
 def get_project_root() -> Path:
     path = Path(__file__).parent.parent.parent
     logger.log(5, f'fetched project root: {path}')
-    return Path(__file__).parent.parent.parent
+    return path
